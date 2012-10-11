4678046d96429065db2d47445fc1ac16427ea495
Jaime Bueza <jbueza@gmail.com>
2012-10-11 15:09:52 -0700

    Adding more to express example

--- examples/express-web-app/routes/index.js
+++ examples/express-web-app/routes/index.js
@@ -1,8 +1,16 @@
+var TwitchClient = require("node-twitchtv");
+
+var client = new TwitchClient();
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  
+  client.games({ limit: 20 }, function(err, games) {
+    console.log(games[0].game);
+    res.render('index', { title: 'Twitch Express Games List', games: games });
+  });
+  
 };
