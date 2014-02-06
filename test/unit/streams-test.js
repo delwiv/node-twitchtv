7d74e02300574ae7b737fd3aecbfbef65bd9474a
Jaime Bueza <jbueza@gmail.com>
2014-02-06 06:56:26 -0800

    Remove unused tests

--- test/unit/streams-test.js
+++ test/unit/streams-test.js
@@ -2,17 +2,17 @@ var TwitchClient = require("../../node-twitchtv")
   , should = require("should")
   , account = require("../conf/example.json");
 
-describe("Channels", function() {
+describe("Streams", function() {
   
   var client = new TwitchClient(account);
   
   it("should return false if channel is missing in params", function() {
-    client.users({}, true).should.be.false;
+    client.streams({}, true).should.be.false;
   });
   it("should return false if callback isn't passed", function() {
-    client.users({ channel: "awesome" }).should.be.false;
+    client.streams({ channel: "koolbeans" }).should.be.false;
   });
   it("should return false if callback is not a function", function() {
-    client.users({ channel: "awesome" }, true).should.be.false;
+    client.streams({ channel: "koolcat" }, true).should.be.false;
   });
 });
